04cfc352596896cea5f287fdd21f87900348aff8
Udara Seneviratne <udara@192.168.1.106>
2018-02-20 15:29:51 +0530

    test site upload

---
+++ django_samples/mysite/polls/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+# Create your views here.
+
+from .models import Question
+from .models import Choice
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question':question,
+			'error_message':'You did not select a Choice',
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
